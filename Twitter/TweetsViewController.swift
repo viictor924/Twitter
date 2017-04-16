87a2fbec096d08c678fe4383f750308e2a2ad7bf
Victor Rodriguez <viictor.rodriguez@gmail.com>
2017-04-15 21:46:04 -0700

    User can pull to refresh

--- Twitter/TweetsViewController.swift
+++ Twitter/TweetsViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 import AFNetworking
 
 class TweetsViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
-
-  
+    
+    
     @IBOutlet weak var tableView: UITableView!
     
     var tweets: [Tweet]!
+    var refreshControl: UIRefreshControl?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,33 +24,39 @@ class TweetsViewController: UIViewController,UITableViewDelegate, UITableViewDat
         tableView.delegate = self
         configureRowHeight()
         
+        addRefreshControl()
+        requestHomeTimeline()
+    }
+    
+    func requestHomeTimeline() -> Void{
         TwitterClient.sharedInstance?.homeTimeline(success: { (tweets:[Tweet]) in
             self.tweets = tweets
             print("printing all the tweets")
             self.tableView.reloadData()
-
+            
         }, failure: { (error: NSError) in
             print(error.localizedDescription)
         })
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // ============ IB Action Methods ========================
     @IBAction func onLogoutButton(_ sender: Any) {
         
         print("User clicked the logOut button")
         TwitterClient.sharedInstance?.logOut()
         User.currentUser = nil
     }
+    // ========================================================
     // ============ Table View Methods ========================
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as! TweetCell
         cell.tweet = tweets[indexPath.row]
-
+        
         return cell
     }
     
@@ -60,21 +67,44 @@ class TweetsViewController: UIViewController,UITableViewDelegate, UITableViewDat
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    // ========================================================
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     fileprivate func configureRowHeight() {
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
     }
+    
+    // ========================================================
+    // ============ Refresh Control Methods ========================
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    func refresh(sender:AnyObject)
+    {
+        requestHomeTimeline()
+        print("end refreshing")
+        self.refreshControl?.endRefreshing()
+    }
+    
+    fileprivate func addRefreshControl() -> Void{
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(TweetsViewController.refresh), for: UIControlEvents.valueChanged)
+        self.refreshControl = refreshControl
+        // add refresh control to table view
+        tableView.insertSubview(refreshControl, at: 0)
+    }
+    // ========================================================
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
